3745c9eb1698d0f8c9c2ca2e148f0e9ca915c937
Tushar-Brn <86977765+Tushar-Brn@users.noreply.github.com>
2021-07-18 19:54:16 +0530

    Add files via upload

---
+++ JavaApplication3.java.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author TUSHAR
+ */
+public class JavaApplication3 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) 
+    {
+        int a[][]={{1,3,4},{2,4,3},{3,4,5}};
+        int b[][]={{1,3,4},{2,4,3},{1,2,4}};
+        int c[][]=new int[3][3];
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                c[i][j]=0;
+                for(int k=0;k<3;k++)
+                {
+                    c[i][j]+=a[i][k]*b[k][j];
+                }
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }
+        // TODO code application logic here
+    }
+    
+}
---
+++ JavaApplication4.java.java
@@ -0,0 +1,5 @@
+run:
+2 6 8 
+4 8 6 
+4 6 9 
+BUILD SUCCESSFUL (total time: 0 seconds)
